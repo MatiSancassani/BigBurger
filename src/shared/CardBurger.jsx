ced43914dc55e1ee7e33b16eb6b593c91f40f4a3
Sanca <matiassancassani@gmail.com>
2025-03-17 00:07:41 -0300

    Arreglos carga de imagen

--- src/shared/CardBurger.jsx
+++ src/shared/CardBurger.jsx
@@ -6,7 +6,7 @@ const CardBurger = ({ title, thumbnail, id }) => {
         <div className="min-h-[20rem] w-[20rem] flex flex-col items-center justify-between rounded-xl text-white mb-[1rem] p-[0.5rem]">
             <div className="">
                 <Link to={`/burger/${id}`}>
-                    <img className="lg:w-[20rem] lg:hover:translate-y-[-5px] lg:transition-transform lg:duration-[0.3s] lg:ease-in-out" src={`https://bigburgerbackend-1.onrender.com${thumbnail}`} alt={title} />
+                    <img className="lg:w-[20rem] lg:hover:translate-y-[-5px] lg:transition-transform lg:duration-[0.3s] lg:ease-in-out" src={`${thumbnail}`} alt={title} />
                 </Link>
             </div>
             <div className="">
